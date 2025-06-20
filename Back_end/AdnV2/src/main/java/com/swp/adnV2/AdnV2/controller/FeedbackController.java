41e18f2a0bb15407d20fc6c986f6f92562757ef0
TaiNguyenTN <nguyenquoctai2022nt@gmail.com>
2025-06-20 17:48:51 +0700

    Add Authorize

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/controller/FeedbackController.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/controller/FeedbackController.java
@@ -4,6 +4,7 @@ import com.swp.adnV2.AdnV2.dto.FeedbackRequest;
 import com.swp.adnV2.AdnV2.service.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +17,13 @@ public class FeedbackController {
 
 
     @GetMapping("/search/by-service-name/{serviceName}")
+    @PreAuthorize("hasAnyRole('GUEST', 'CUSTOMER', 'STAFF', 'MANAGER')")
     public ResponseEntity<?> searchFeedbacksByServiceName(@PathVariable String serviceName, @RequestParam(required = false) String keyword) {
         return feedbackService.searchFeedback(serviceName, keyword);
     }
 
     @PostMapping("/create/{serviceId}")
+    @PreAuthorize("hasAnyRole('CUSTOMER', 'STAFF', 'MANAGER')")
     public ResponseEntity<?> createFeedback(@PathVariable Long serviceId,@RequestBody FeedbackRequest feedbackRequest) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
@@ -28,6 +31,7 @@ public class FeedbackController {
     }
 
     @PutMapping("/update/{feedbackId}")
+    @PreAuthorize("hasAnyRole('CUSTOMER', 'STAFF', 'MANAGER')")
     public ResponseEntity<?> updateFeedback(@PathVariable Long feedbackId, @RequestBody FeedbackRequest feedbackRequest) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
@@ -35,6 +39,7 @@ public class FeedbackController {
     }
 
     @DeleteMapping("/delete/{feedbackId}")
+    @PreAuthorize("hasAnyRole('STAFF', 'MANAGER')")
     public ResponseEntity<?> deleteFeedback(@PathVariable Long feedbackId) {
         return feedbackService.deleteFeedback(feedbackId);
     }
