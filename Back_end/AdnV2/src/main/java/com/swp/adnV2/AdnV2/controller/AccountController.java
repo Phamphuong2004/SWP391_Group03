cb8eb8884f36a38031a8f1d2c0a0b1bb21247767
cuongpk-create <cuongpkse183902@fpt.edu.vn>
2025-06-22 19:52:18 +0700

    Update code

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/controller/AccountController.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/controller/AccountController.java
@@ -1,4 +1,31 @@
 package com.swp.adnV2.AdnV2.controller;
 
+@RestController
+@RequestMapping("/api/manager/accounts")
+@PreAuthorize("hasRole('MANAGER')")
 public class AccountController {
+    @Autowired
+    private ManagerAccountService accountService;
+
+    @GetMapping
+    public ResponseEntity<List<ManageAccount>> getAllAccounts() {
+        return ResponseEntity.ok(accountService.getAllAccounts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ManageAccount> getAccount(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ManageAccount> updateAccount(@PathVariable Long id,
+                                                       @RequestBody ManageAccount account) {
+        return ResponseEntity.ok(accountService.updateAccount(id, account));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.noContent().build();
+    }
 }
