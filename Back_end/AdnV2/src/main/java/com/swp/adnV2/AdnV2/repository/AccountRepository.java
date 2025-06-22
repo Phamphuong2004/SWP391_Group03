6acfe0852885b3515e2ef754e2322caa4b90e719
cuongpk-create <cuongpkse183902@fpt.edu.vn>
2025-06-22 19:40:29 +0700

    Manage Account

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/repository/AccountRepository.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/repository/AccountRepository.java
@@ -1,4 +1,10 @@
 package com.swp.adnV2.AdnV2.repository;
 
-public interface AccountRepository {
+import com.swp.adnV2.AdnV2.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Users, Long> {
+    Optional<ManageAccount> findByUsername(String username);
 }
