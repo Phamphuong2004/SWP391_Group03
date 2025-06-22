a2eb2042648fd7c2f0b5d1f9ae1e13f2024748e1
cuongpk-create <cuongpkse183902@fpt.edu.vn>
2025-06-22 19:46:08 +0700

    Update code

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/service/ServiceService.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/service/ServiceService.java
@@ -1,7 +1,7 @@
 package com.swp.adnV2.AdnV2.service;
 import com.swp.adnV2.AdnV2.dto.ServiceCreationRequest;
 import com.swp.adnV2.AdnV2.dto.ServiceUpdateRequest;
-import com.swp.adnV2.AdnV2.entity.Service1;
+import com.swp.adnV2.AdnV2.entity.Services;
 import com.swp.adnV2.AdnV2.repository.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,9 +14,9 @@ public class ServiceService {
     @Autowired
     private ServiceRepository serviceRepository;
 
-    public Service1 createRequest(ServiceCreationRequest request){
+    public Services createRequest(ServiceCreationRequest request){
         // Create a new Service entity from the request
-        Service1 service1 = new Service1();
+        Services service1 = new Services();
         service1.setServiceName(request.getServiceName());
         service1.setDescription(request.getDescription());
         service1.setPrice(request.getPrice());
@@ -27,7 +27,7 @@ public class ServiceService {
     }
 
     public Service1 updateService( Long service_id, ServiceUpdateRequest request){
-          Service1 service1 = getServiceById(service_id);
+          Services service1 = getServiceById(service_id);
         // Update the service entity with the new values from the request
           service1.setServiceName(request.getServiceName());
           service1.setDescription(request.getDescription());
