aba729b25e6f3d8cd51ed1d74f423482538b5243
cuongpk-create <cuongpkse183902@fpt.edu.vn>
2025-06-22 20:11:00 +0700

    Update code

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/controller/ServiceAPI.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/controller/ServiceAPI.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 
-@PreAuthorize("hasRole('MANAGER')")
+
 @RestController
 @RequestMapping("/api/services")
 public class ServiceAPI {
@@ -29,12 +29,12 @@ public class ServiceAPI {
     Service1 getServiceById(@PathVariable("service_id") Long serviceId) {
         return serviceService.getServiceById(serviceId);
     }
-
+    @PreAuthorize("hasRole('MANAGER')")
     @PutMapping("/{service_id}")
     Service1 updateService(@PathVariable Long service_id, @RequestBody ServiceUpdateRequest request){
         return serviceService.updateService(service_id, request);
     }
-
+    @PreAuthorize("hasRole('MANAGER')")
     @DeleteMapping("/{service_id}")
     String deleteService(@PathVariable Long service_id) {
         serviceService.deleteService(service_id);
