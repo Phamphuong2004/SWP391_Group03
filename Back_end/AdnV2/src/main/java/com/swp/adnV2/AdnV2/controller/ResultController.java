7af270366b1833199acbbb107299a9525cde9de6
cuongpk-create <cuongpkse183902@fpt.edu.vn>
2025-06-30 18:21:51 +0700

    sửa code  by tùng

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/controller/ResultController.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/controller/ResultController.java
@@ -6,6 +6,7 @@ import com.swp.adnV2.AdnV2.dto.ResultUpdateRequest;
 import com.swp.adnV2.AdnV2.entity.Result;
 import com.swp.adnV2.AdnV2.service.ResultService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,26 +17,31 @@ public class ResultController {
     private ResultService resultService;
 
     @PostMapping("/create")
+    @PreAuthorize("hasAnyRole('STAFF', 'MANAGER')")
     ResultReponse createRequest(@RequestBody ResultCreationRequest request) {
         return resultService.createResult(request);
     }
 
     @GetMapping("/getList")
+    @PreAuthorize("hasAnyRole('STAFF', 'MANAGER')")
     public List<ResultReponse> getAllResults() {
         return resultService.getAllResults();
     }
 
     @GetMapping("/{result_id}")
+    @PreAuthorize("hasAnyRole('CUSTOMER', 'STAFF', 'MANAGER')")
     public ResultReponse getResultById(@PathVariable("result_id") Long resultId) {
         return resultService.getResultById(resultId);
     }
 
     @PutMapping("/{result_id}")
+    @PreAuthorize("hasAnyRole('STAFF', 'MANAGER')")
     public ResultReponse updateResult(@PathVariable Long result_id, @RequestBody ResultUpdateRequest request) {
         return resultService.updateResult(result_id, request);
     }
 
     @DeleteMapping("/{result_id}")
+    @PreAuthorize("hasAnyRole('STAFF', 'MANAGER')")
     public String deleteResult(@PathVariable Long result_id) {
         resultService.deleteResult(result_id);
         return "Result has been deleted successfully";
