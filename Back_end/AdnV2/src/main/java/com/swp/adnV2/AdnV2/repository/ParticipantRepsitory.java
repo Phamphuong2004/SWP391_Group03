000a583222b604d10bac1ed3dc828ecc9cabd188
TaiNguyenTN <nguyenquoctai2022nt@gmail.com>
2025-06-30 23:35:59 +0700

    CRUD Participant

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/repository/ParticipantRepsitory.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/repository/ParticipantRepsitory.java
@@ -1,9 +1,11 @@
 package com.swp.adnV2.AdnV2.repository;
 
 import com.swp.adnV2.AdnV2.entity.Participant;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ParticipantRepsitory extends JpaRepository<Participant, Long> {
+    Participant findByParticipantId(Long participantId);
+    Participant findByPhone(String phone);
+    Participant findByEmail(String email);
 }
