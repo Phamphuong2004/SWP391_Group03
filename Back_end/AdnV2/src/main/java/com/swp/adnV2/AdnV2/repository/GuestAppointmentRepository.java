feda944dec677a1fb471a770a52b3ee3ff0122d3
cuongpk-create <cuongpkse183902@fpt.edu.vn>
2025-07-02 11:16:42 +0700

    sửa code  by tùng

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/repository/GuestAppointmentRepository.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/repository/GuestAppointmentRepository.java
@@ -2,6 +2,8 @@ package com.swp.adnV2.AdnV2.repository;
 
 import com.swp.adnV2.AdnV2.entity.GuestAppointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GuestAppointmentRepository extends JpaRepository<GuestAppointment, Long> {
 }
--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/repository/SampleRepository.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/repository/SampleRepository.java
@@ -4,9 +4,10 @@ import com.swp.adnV2.AdnV2.entity.Sample;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface SampleRepository extends JpaRepository<Sample, Long> {
     // Các phương thức liên quan đến Sample
     List<Sample> findByAppointment_AppointmentId(Long appointmentId);
