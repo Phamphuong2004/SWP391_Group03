e679b5c1acdef91eb6ece9838816d0072a8c17f7
TaiNguyenTN <nguyenquoctai2022nt@gmail.com>
2025-07-14 17:04:19 +0700

    Commit

--- Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/entity/SampleType.java
+++ Back_end/AdnV2/src/main/java/com/swp/adnV2/AdnV2/entity/SampleType.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class SampleType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sample_type_id")
     private Long id;
 
     @Column(name = "name", columnDefinition = "NVARCHAR(255)", unique = true)
